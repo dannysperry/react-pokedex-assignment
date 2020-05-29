d80e32b9210dc3845f3449dc460b185246633ffa
VadimS4 <github email address>
2020-05-29 00:01:15 -0700

    created the 2 components, and added react-router

---
+++ pokedex/src/components/Detail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styling/detail.css';
+
+const Detail = () => {
+    return(
+        <div className="detail">    
+        </div>
+    )
+}
+
+export default Detail;
